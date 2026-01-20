c3176b34e8c4850948aa6f003438da665c7241ee
Edgar Mendes <mendesbr92@gmail.com>
2026-01-20 20:32:06 -0300

    feat: adicionado o mapper para conversao de tipos

---
+++ src/main/java/com/br/mapper/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.br.mapper;
+
+import com.br.domain.Product;
+import com.br.dto.ProductRequestDTO;
+import com.br.dto.ProductResponseDTO;
+
+public class ProductMapper {
+
+    private ProductMapper() {} // impede new ProductMapper()
+
+    public static Product toEntity(ProductRequestDTO request) {
+
+        return new Product(
+
+                request.getName(), request.getDescription(), request.getPrice());
+    }
+
+    public static ProductResponseDTO toResponse(Product response) {
+
+        return new ProductResponseDTO(
+
+                response.getId(), response.getName(), response.getDescription(),
+                response.getPrice(), response.getActive(), response.getCreatedAt());
+    }
+}
