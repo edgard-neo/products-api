d91b3c22f954231b983b33bf7e5389e3cfb9f4a1
Edgar Mendes <mendesbr92@gmail.com>
2026-01-23 23:17:35 -0300

    feat: adicionado descriçoes do swegger

--- src/main/java/com/br/dto/ProductRequestDTO.java
+++ src/main/java/com/br/dto/ProductRequestDTO.java
@@ -1,11 +1,13 @@
 package com.br.dto;
 
 import java.math.BigDecimal;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "Dados para criar ou atualizar o produto")
 public class ProductRequestDTO {
 
     @NotBlank(message = "Nome é obrigatorio")
